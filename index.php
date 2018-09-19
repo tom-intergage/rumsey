2c78a70c7d6a5e03f068f893f0be9ce7213b8930
Tom Aylott <taylott@intergage.co.uk>
2018-09-19 13:13:55 +0100

    upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+header("Access-Control-Allow-Origin: *");
+
+$params = $_GET;
+if ($params) {
+    $odta = "bVQ%3DGMJE%26PVQ%3D-E";
+    $url = 'http://hbofeeds.booking-system.net/HBO_Availability_XML.asp?odta='.$odta;
+    $xml = simplexml_load_file($url);
+    $json = json_encode($xml,JSON_PRETTY_PRINT);
+    print_r($json);
+}
+else {
+    echo "{\"Fatal Error\":\"No Parameters Supplied\"}";
+}
+
+?>
