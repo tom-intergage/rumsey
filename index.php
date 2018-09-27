923a24cd9da19def626e0ca5b0bbf3037a149bb9
Tom Aylott <taylott@intergage.co.uk>
2018-09-27 16:57:39 +0100

    Delivery

--- index.php
+++ index.php
@@ -1,18 +1,35 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
-header("Access-Control-Allow-Origin: http://rumseyofsandbankscouk.14-1.a01.co.uk");
-
+header("Access-Control-Allow-Origin: *");
+$error = false;
 $params = $_GET;
-if ($params) {
-    $odta = "odta=bVQ%3DGMJE%26PVQ%3D-E";
 
-    $url = 'http://hbofeeds.booking-system.net/HBO_Availability_XML.asp?'.$odta;
-    $xml = simplexml_load_file($url);
-    $json = json_encode($xml,JSON_PRETTY_PRINT);
-    print_r($json);
-}
-else {
-    echo "{\"Error\":\"No Parameters Supplied\"}";
+if ($params['view']) :
+    $odta = "bVQ%3DGMJE%26PVQ%3D-E";
+    $urlBase = "http://hbofeeds.booking-system.net/";
+    if ($params['view'] == 'availability') :
+        $url = $urlbase . 'HBO_Availability_XML.asp?odta='.$odta;
+    
+    elseif ($params['view'] == 'offers') :
+        $url = $urlbase . 'HBO_Prices_XML.asp?odta='.$odta;
+
+    else :
+        $error == true;
+    
+    endif;
+
+    if (!$error) :
+        $xml = simplexml_load_file($url);
+        $json = json_encode($xml,JSON_PRETTY_PRINT);
+        print_r($json);
+    endif;
+
+else :
+    $error = true;
+endif;
+
+if ($error) {
+    echo "{\"Error\":\"No Views Specified\"}";
 }
 
 ?>
